c0b2e0d119a6a347a6d829b1f1b30b87c7d64726
ArvindChaturvedi <Arvind.Chaturvedi1092@gmail.com>
2025-01-20 12:37:59 +0530

    Updating next.config.js and github workflow

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/Arvind-Portfolio', // Replace with your repository name
+  images: { unoptimized: true },
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
